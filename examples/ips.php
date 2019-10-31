0219cd3747679644b6e4f0aa4f91569111c8600b
James Wade <jpswade@gmail.com>
2019-10-31 21:33:40 +0000

    feat: add a ips example

---
+++ examples/ips.php
@@ -0,0 +1,24 @@
+<?php
+
+use Deaduseful\MyServersApi\MyServersApi;
+
+$api = new MyServersApi(MyServersApi::LIVE_URL, getenv('MYSERVERS_API_USER'), getenv('MYSERVERS_API_PASS'));
+$servers = MyServersApi::arrayHelper($api->GetAllServerDetails($api->getParams())->GetAllServerDetailsResult, 'ServerInfo');
+$list = [];
+foreach ($servers as $server) {
+    $id = $server->ServiceID;
+    $primaryIp = $server->PrimaryIP;
+    $reverseDns = MyServersApi::arrayHelper($server->ReverseDnsEntries, 'ReverseDnsEntry');
+    foreach ($reverseDns as $entry) {
+        $ip = $entry->IPAddress;
+        $host = $entry->HostName;
+        if ($ip == $primaryIp) {
+            $id = $host;
+        }
+    }
+    $ips = MyServersApi::arrayHelper($server->IPAddresses, 'string');
+    foreach ($ips as $ip) {
+        $list[$ip] = $id;
+    }
+}
+var_dump($list);
