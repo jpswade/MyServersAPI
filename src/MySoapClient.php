72e97f32776ec9aeacab7b07b190cab75887de7b
James Wade <jpswade@gmail.com>
2022-02-23 20:49:22 +0000

    fix: compat

--- src/MySoapClient.php
+++ src/MySoapClient.php
@@ -7,20 +7,21 @@ use SoapFault;
 
 class MySoapClient extends SoapClient
 {
+    private $maximum_retries = 5;
+
     /**
-     * @param string $function_name
-     * @param array $arguments
-     * @param int $maximum_retries
+     * @param string $name
+     * @param mixed $args
      * @return false|mixed
      * @throws SoapFault
      */
-    public function __call($function_name, $arguments, $maximum_retries = 5)
+    public function __call($name, array $args)
     {
         $result = false;
         $retryCount = 0;
-        while (!$result && $retryCount < $maximum_retries) {
+        while (!$result && $retryCount < $this->maximum_retries) {
             try {
-                $result = parent::__call($function_name, $arguments);
+                $result = parent::$name($args);
             } catch (SoapFault $fault) {
                 if ($fault->faultstring != 'Could not connect to host') {
                     throw $fault;
@@ -29,8 +30,8 @@ class MySoapClient extends SoapClient
             sleep(1);
             $retryCount++;
         }
-        if ($retryCount >= $maximum_retries) {
-            throw new SoapFault(508, sprintf('Could not connect to host after %d attempts', $maximum_retries));
+        if ($retryCount >= $this->maximum_retries) {
+            throw new SoapFault(508, sprintf('Could not connect to host after %d attempts', $this->maximum_retries));
         }
         return $result;
     }
